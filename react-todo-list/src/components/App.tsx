f182c02463f57022f08ce78a5854d0719266f333
Rozzini <Rozzinimr@gmail.com>
2020-12-16 16:49:48 +0300

    tutor 2 part 3 completed

---
+++ react-todo-list/src/components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class App extends React.Component<IProps, {}> {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+interface IProps { name: string; }
