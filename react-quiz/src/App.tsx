625be373857c13a2a87e2483ac42c5d01467003c
Rozzini <Rozzinimr@gmail.com>
2020-12-17 16:33:48 +0300

    Tutorial 48:30

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,8 +1,90 @@
-import React from 'react';
+//types
+import { Difficulty, QuestionState } from './API';
+import React, {useState} from 'react';
+
+//Components
+import QuestionCard from './components/QuestionCard';
+import { fetchQuizQuestions } from './API';
+
+type AnswerObject = {
+	question: string;
+	answer: string;
+	correct: boolean;
+	correctAnswer: string;
+}
+
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
+
+	const [loading, setLoading] = useState(false);
+	const [questions, setQuestions] = useState<QuestionState[]>([]);
+	const [number, setNumber] = useState(0);
+	const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+	const [score, setScore] = useState(0);
+	const [gameOver, setGameOver] = useState(true);
+
+
+	console.log(questions);
+
+
+	const startTrivia = async () => {
+			setLoading(true);
+			setGameOver(false);
+
+			const newQuestions = await fetchQuizQuestions(
+				TOTAL_QUESTIONS,
+				Difficulty.MEDIUM
+			);
+
+			setQuestions(newQuestions);
+			setScore(0);
+			setUserAnswers([]);
+			setNumber(0);
+			setLoading(false);
+	}
+
+	const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+	}
+
+	const nextQuestion = () => {
+
+	}
+
   return (
-    <div className="App">Quiz</div>
+  <div className="App">
+		<h1> REACT QUIZ</h1>
+		{gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+			<button className="start" onClick={startTrivia}>
+			Start
+		</button>
+		) : null}
+
+
+		{!gameOver ? <p className="Score">Score:</p> : null }
+
+		{loading && <p>Loading Questions ...</p>}
+
+		{!loading && !gameOver && (
+		<QuestionCard
+			questionNr={number + 1}
+			totalQuestions= {TOTAL_QUESTIONS}
+			question={questions[number].question}
+			answers={questions[number].answers}
+			userAnswer={userAnswers ? userAnswers[number] : undefined}
+			callback={checkAnswer}
+		/>
+		)}
+
+		{!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+		<button className="next" onClick={nextQuestion}>
+			Next Question
+		</button>
+		) : null}
+
+  </div>
   );
 }
 
